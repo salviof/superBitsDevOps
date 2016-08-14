a306ce5f6de4415eb184126247f009ab16379bb5
Salvio Furbino <salviof@gmail.com>
2016-08-14 11:54:07 -0300

    Atualizacao automática

--- VARIAVEIS/SB_VARIAVEIS_MAVEN_GIT.sh
+++ VARIAVEIS/SB_VARIAVEIS_MAVEN_GIT.sh
@@ -1,7 +1,6 @@
 ARGUMENTOS_ESPERADOS=1
 
 PASTA_PROJETO=$1
-source /home/superBits/superBitsDevOps/VARIAVEIS/SB_VARIAVEIS.sh
 # Verificando se o o Cliente e o Projeto foram enviados
 if [ $# -ne $ARGUMENTOS_ESPERADOS ]
 then
@@ -12,7 +11,12 @@ fi
 #ATENÇÃO VARIAVEL DUPLICADA EM SB_VARIAVEIS
 declare -a PASTAS_MODULO=("modelRegras" "webApp" "Android" "ws" "ic")
 declare -a CAMINHOS_TODOS_PROJETOS=($PASTA_PROJETO)
-declare -a CAMINHOS_TUDO=()
+declare -a CAMINHOS_TUDO=("CAMINHO TESTE1" "CAMINHOTESTE2" "CAMNHO TESTE3" "CAMINHO TESTE4")
 declare -a CAMINHOS_MODEL=()
 declare -a CAMINHOS_FRAMEWORK=()
 
+
+if [[ "$PASTA_PROJETO" == *TESTE ]]
+then
+CAMINHOS_FRAMEWORK+=("${!CAMINHOS_TUDO[@]}")
+fi
