592cd3d05103a0fe093fad57b61dfea7c6fffeec
Salvio Furbino <salvioF@salvio.marketingparaweb.com.br>
2016-09-21 17:27:39 -0300

    Atualizacao automática

--- SCRIPTS_SERVIDOR/atualizarProjetos.sh
+++ SCRIPTS_SERVIDOR/atualizarProjetos.sh
@@ -5,36 +5,42 @@ while read linha
 do
     projetos[ $idx ]="$linha"        
     (( idx++ ))
-done < <(ls ~/projetos/)
+done < <(ls ~/gitServer/release/)
 cd /home
 for proj in "${projetos[@]}"
 do :
+
+nomeProjeto=$(echo $chave_url | sed -e 's/^.git//')
 # Faz um Git clone do repositorio na pasta publicados
 mkdir ~/publicados/ -p
 cd ~/publicados
 git clone ~/gitServer/release/$proj
-cd ~/publicados/$proj
+
+
+cd ~/publicados/$nomeProjeto
 git pull orign master
 
 #Lê as informacoes do cliente (contendo o endereço do site que será homologado)
-source ~/publicados/$proj/cliente.info
-source ~/publicados/$proj/$proj.info
+source ~/publicados/$nomeProjeto/cliente.info
+source ~/publicados/$nomeProjeto/$nomeProjeto.info
 
 echo " Atualizando  $SERVIDOR_HOMOLOGACAO"
 echo " e  $SERVIDOR_REQUISITOS"
 
-arqProjRequisito="$proj.req.xml"
+
+
+arqProjRequisito="$nomeProjeto.req.xml"
 # Adiciona o contecto no Jetty	
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?> "> ~/servidor/jetty9/webapps/$proj.xml
-echo " <Configure class=\"org.eclipse.jetty.webapp.WebAppContext\"> " >>  ~/servidor/jetty9/webapps/$proj.xml
-echo "   <Set name=\"contextPath\">/</Set> " >>  ~/servidor/jetty9/webapps/$proj.xml
-echo "   <Set name=\"war\">/home/git/publicados/$proj/$proj.war</Set> " >>  ~/servidor/jetty9/webapps/$proj.xml
-echo "   <Set name=\"virtualHosts\">            " >>  ~/servidor/jetty9/webapps/$proj.xml
-echo "     <Array type=\"java.lang.String\">    " >>  ~/servidor/jetty9/webapps/$proj.xml
-echo "       <Item>$SERVIDOR_HOMOLOGACAO</Item>  " >>  ~/servidor/jetty9/webapps/$proj.xml
-echo "     </Array>  " >>  ~/servidor/jetty9/webapps/$proj.xml
-echo "   </Set> " >>  ~/servidor/jetty9/webapps/$proj.xml
-echo " </Configure> " >>  ~/servidor/jetty9/webapps/$proj.xml
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?> "> ~/servidor/jetty9/webapps/$nomeProjeto.xml
+echo " <Configure class=\"org.eclipse.jetty.webapp.WebAppContext\"> " >>  ~/servidor/jetty9/webapps/$nomeProjeto.xml
+echo "   <Set name=\"contextPath\">/</Set> " >>  ~/servidor/jetty9/webapps/$nomeProjeto.xml
+echo "   <Set name=\"war\">/home/git/publicados/$proj/$proj.war</Set> " >>  ~/servidor/jetty9/webapps/$nomeProjeto.xml
+echo "   <Set name=\"virtualHosts\">            " >>  ~/servidor/jetty9/webapps/$nomeProjeto.xml
+echo "     <Array type=\"java.lang.String\">    " >>  ~/servidor/jetty9/webapps/$nomeProjeto.xml
+echo "       <Item>$SERVIDOR_HOMOLOGACAO</Item>  " >>  ~/servidor/jetty9/webapps/$nomeProjeto.xml
+echo "     </Array>  " >>  ~/servidor/jetty9/webapps/$nomeProjeto.xml
+echo "   </Set> " >>  ~/servidor/jetty9/webapps/$nomeProjeto.xml
+echo " </Configure> " >>  ~/servidor/jetty9/webapps/$nomeProjeto.xml
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?> "> ~/servidor/jetty9/webapps/$arqProjRequisito
 echo " <Configure class=\"org.eclipse.jetty.webapp.WebAppContext\"> " >>  ~/servidor/jetty9/webapps/$arqProjRequisito
