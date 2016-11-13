491a3bf00810576046cd33ea582e4cb5c6362828
Salvio Furbino <salvioF@salvio.marketingparaweb.com.br>
2016-11-13 21:38:45 -0200

    Atualizacao automática

--- SCRIPTS_SERVIDOR/criarBancoDeDados.sh
+++ SCRIPTS_SERVIDOR/criarBancoDeDados.sh
@@ -38,9 +38,9 @@ echo " e  $SERVIDOR_REQUISITOS"
 mysqladmin processlist -u root $NOME_BANCO | \
 awk '$2 ~ /^[0-9]/ {print "KILL "$2";"}' | \
 
-mysqladmin -u root drop $NOME_BANCO
+mysqladmin -u root drop $NOME_BANCO -f
 echo "criando $NOME_BANCO"
-mysqladmin -u root create $NOME_BANCO
+mysqladmin -u root create $NOME_BANCO -f
 
-mysqk -u root  $NOME_BANCO < ./bancoHomologacao.sql
+mysql -u root  $NOME_BANCO < ./bancoHomologacao.sql
 
