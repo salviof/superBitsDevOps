d6d2afce81e1ae8b0670a41805b6b8cf5c80cf7b
Salvio Furbino <salvioF@salvio.marketingparaweb.com.br>
2016-11-13 22:15:37 -0200

    Atualizacao automática

--- SCRIPTS_SERVIDOR/atualizarProjeto.sh
+++ SCRIPTS_SERVIDOR/atualizarProjeto.sh
@@ -46,23 +46,23 @@ echo " e  $SERVIDOR_REQUISITOS"
 
 
 # Adiciona o contecto no Jetty	
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?> "> ~/servidor/jetty9/webapps/$NOME_PROJETO.xml
-echo " <Configure class=\"org.eclipse.jetty.webapp.WebAppContext\"> " >>  ~/servidor/jetty9/webapps/$NOME_PROJETO.xml
-echo "   <Set name=\"contextPath\">/</Set> " >>  ~/servidor/jetty9/webapps/$NOME_PROJETO.xml
-echo "   <Set name=\"war\">/home/git/publicados/$NOME_PROJETO/$NOME_PROJETO.war</Set> " >>  ~/servidor/jetty9/webapps/$NOME_PROJETO.xml
-echo "   <Set name=\"virtualHosts\">            " >>  ~/servidor/jetty9/webapps/$NOME_PROJETO.xml
-echo "     <Array type=\"java.lang.String\">    " >>  ~/servidor/jetty9/webapps/$NOME_PROJETO.xml
-echo "       <Item>$ENDERECO_WEB</Item>  " >>  ~/servidor/jetty9/webapps/$NOME_PROJETO.xml
-echo "     </Array>  " >>  ~/servidor/jetty9/webapps/$NOME_PROJETO.xml
-echo "   </Set> " >>  ~/servidor/jetty9/webapps/$NOME_PROJETO.xml
-echo " </Configure> " >>  ~/servidor/jetty9/webapps/$NOME_PROJETO.xml
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?> "> ~/servidor/jetty9/webapps/$GRUPO_PROJETO.xml
+echo " <Configure class=\"org.eclipse.jetty.webapp.WebAppContext\"> " >>  ~/servidor/jetty9/webapps/$GRUPO_PROJETO.xml
+echo "   <Set name=\"contextPath\">/</Set> " >>  ~/servidor/jetty9/webapps/$GRUPO_PROJETO.xml
+echo "   <Set name=\"war\">/home/git/publicados/$NOME_PROJETO/$GRUPO_PROJETO.war</Set> " >>  ~/servidor/jetty9/webapps/$NOME_PROJETO.xml
+echo "   <Set name=\"virtualHosts\">            " >>  ~/servidor/jetty9/webapps/$GRUPO_PROJETO.xml
+echo "     <Array type=\"java.lang.String\">    " >>  ~/servidor/jetty9/webapps/$GRUPO_PROJETO.xml
+echo "       <Item>$ENDERECO_WEB</Item>  " >>  ~/servidor/jetty9/webapps/$GRUPO_PROJETO.xml
+echo "     </Array>  " >>  ~/servidor/jetty9/webapps/$GRUPO_PROJETO.xml
+echo "   </Set> " >>  ~/servidor/jetty9/webapps/$GRUPO_PROJETO.xml
+echo " </Configure> " >>  ~/servidor/jetty9/webapps/$GRUPO_PROJETO.xml
 
-ARQ_PROJ_REQUISITO=$NOME_PROJETO.req.xml
+ARQ_PROJ_REQUISITO=$GRUPO_PROJETO.req.xml
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?> "> ~/servidor/jetty9/webapps/$ARQ_PROJ_REQUISITO
 echo " <Configure class=\"org.eclipse.jetty.webapp.WebAppContext\"> " >>  ~/servidor/jetty9/webapps/$ARQ_PROJ_REQUISITO
 echo "   <Set name=\"contextPath\">/</Set> " >>  ~/servidor/jetty9/webapps/$ARQ_PROJ_REQUISITO
-echo "   <Set name=\"war\">/home/git/publicados/$proj/$proj.req.war</Set> " >>  ~/servidor/jetty9/webapps/$ARQ_PROJ_REQUISITO
+echo "   <Set name=\"war\">/home/git/publicados/$GRUPO_PROJETO/$GRUPO_PROJETO.req.war</Set> " >>  ~/servidor/jetty9/webapps/$ARQ_PROJ_REQUISITO
 echo "   <Set name=\"virtualHosts\">            " >>  ~/servidor/jetty9/webapps/$ARQ_PROJ_REQUISITO
 echo "     <Array type=\"java.lang.String\">    " >>  ~/servidor/jetty9/webapps/$ARQ_PROJ_REQUISITO
 echo "       <Item>$ENDERECO_WEB_REQUISITO</Item>  " >>  ~/servidor/jetty9/webapps/$ARQ_PROJ_REQUISITO
