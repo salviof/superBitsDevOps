ca462b9220c2085ed1286a93bd4ab23d011fd395
Desenvolvedor <desenvolvedor@localhost.localdomain>
2017-04-27 11:38:58 -0300

    Atualizacao automática

---
+++ baixarSubModulos.sh
@@ -0,0 +1,7 @@
+echo "executando init dos submodulos (caso ainda não tenha sido excutados)"
+git checkout master  && git pull --ff origin master
+git submodule sync
+git submodule init
+git submodule update
+git submodule foreach "(git checkout master && git pull --ff origin master) || true"
+
