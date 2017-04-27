5578ff4f607a4fac120c183a8b0ad8b05e65de71
Desenvolvedor <desenvolvedor@localhost.localdomain>
2017-04-27 11:32:43 -0300

    Atualizacao automática

--- estacaoDeveloperOps/instalarApilicativos.sh
+++ estacaoDeveloperOps/instalarApilicativos.sh
@@ -13,19 +13,25 @@ cd ./aplicativos
 echo "O sistema irá instalar o jdk8 64 bits"
 echo "Em seguida o sistema irá perguntar qual "
 echo "a configuração para o caminho padrão do comando java "
-echo "você deverá informar o NUMERO CORRESPONDENTE ao caminho do  JAVA 1.8 "
 pause
 cd ~
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm
 
-sudo yum install ./jdk-8u101-linux-x64.rpm
+sudo yum install ./jdk-8u102-linux-x64.rpm
+
+echo "você deverá informar o NUMERO CORRESPONDENTE ao caminho do  JAVA 1.8 "
+
 sudo alternatives --config java
 sudo alternatives --config javac
 sudo alternatives --config javaws
 sudo alternatives --config jar
-sudo 
+
+echo " CONFIGURANDO JAVA_HOME "
+
+echo "export JAVA_HOME=/usr/java/lastest" >> ~/.bash_profile
+export JAVA_HOME=/usr/java/lastest     
+
 echo "ATENÇÃO "
-echo "Este Script não configura o JAVA_HOME "
 echo "precione enter para instalar o netbeans 8.1 "
 pause
 cd ~
