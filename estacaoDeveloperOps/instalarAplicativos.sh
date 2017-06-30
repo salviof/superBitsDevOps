ed85bd9d8c042b8fdf7718d6a70d3bbb96fc26d6
Salvio Furbino Elias <salviof@gmail.com>
2017-06-29 23:09:58 -0300

    Atualização atendendendo: Tarefa não Especificada

--- estacaoDeveloperOps/instalarAplicativos.sh
+++ estacaoDeveloperOps/instalarAplicativos.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# init
+#Garante que o interpretador utilizado será BashScript (nescessário em alguns casos)
+# Este script depende do módulo Super-Bits DevOps encontrado em: https://github.com/salviof/superBitsDevOps
+
+# Chamando funções do core
+source /home/superBits/superBitsDevOps/core/coreSBBash.sh
+#
+
+
+
 function pause(){
    read -p "$*"
 }
@@ -10,24 +18,32 @@ function pause(){
 
 echo "Script reservado, instação e ambiente java 1.8 e netbens"
 cd ./aplicativos
-echo "O sistema irá instalar o jdk8 64 bits"
-echo "Em seguida o sistema irá perguntar qual "
-echo "a configuração para o caminho padrão do comando java "
+alerta "O sistema irá instalar o jdk8 64 bits"
+alerta "Em seguida o sistema irá perguntar qual "
+alerta "a configuração para o caminho padrão do comando java através do comando alternatives "
 pause
+ENDERECO_SDK=http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163
+ARQUIVO_SDK=jdk-8u131-linux-x64.rpm
+alerta "Baixando JDK 1.8 64 bits Oracle "]
+alerta "em : $ENDERECO_SDK/$ARQUIVO_SDK"
+
 cd ~
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $ENDERECO_SDK/$ARQUIVO_SDK
 
-sudo yum install ./jdk-8u102-linux-x64.rpm
+cd ~
+arqSairSeArquivoNaoExistir ~/$ARQUIVO_SDK "Houve um erro ao Baixar o arquivo JDK, verifique o novo endereço no site da oracle, atualise este arquivo, e se possível efetue um pull requeste de atualização para colaborar com esta alteração"
 
-echo "você deverá informar o NUMERO CORRESPONDENTE ao caminho do  JAVA 1.8 "
+sudo yum install ~/$ARQUIVO_SDK
 
-sudo alternatives --config java
-sudo alternatives --config javac
-sudo alternatives --config javaws
-sudo alternatives --config jar
+alerta "você deverá informar o NUMERO CORRESPONDENTE ao caminho do  JAVA SKD 1.8 Oracle "
+
+ alternatives --config java
+ alternatives --config javac
+ alternatives --config javaws
+ alternatives --config jar
 
 echo " CONFIGURANDO JAVA_HOME "
-
+alerta "Adicionando caminho para o java home em ~/.bash_profile"
 echo "export JAVA_HOME=/usr/java/lastest" >> ~/.bash_profile
 export JAVA_HOME=/usr/java/latest     
 
@@ -35,8 +51,19 @@ echo "ATENÇÃO "
 echo "precione enter para instalar o netbeans 8.1 "
 pause
 cd ~
-wget http://download.netbeans.org/netbeans/8.0.2/final/bundles/netbeans-8.0.2-javaee-linux.sh
-sudo chmod +x netbeans-8.0.2-javaee-linux.sh
-sudo ./netbeans-8.0.2-javaee-linux.sh
+ENDERECONETBEANS=http://download.netbeans.org/netbeans/8.2/final/bundles/
+ARQUIVONETBEANS=netbeans-8.2-javaee-linux.sh
+alerta "Baixando netbens em: $ENDERECONETBEANS/$ARQUIVONETBEANS"
+wget $ENDERECONETBEANS/$ARQUIVONETBEANS
+arqSairSeArquivoNaoExistir ~/$ARQUIVONETBEANS "O arquivo não foi encontrado, verifique a url correta do netbeans, se possível atualise este scritp e faça um pull request"
+chmod +x $ARQUIVONETBEANS
+cd ~
+./$ARQUIVONETBEANS
+alerta "O sistema irá instalar o Git"
+sudo yum install git
+alerta "O sistema irá instalar o Maven"
+sudo yum install maven
+alerta "O sistema irá instalar o XmlStarlet (manipulador de xml via linha de comando)"
+sudo yum install xmlstarlet
 
 
--- estacaoDeveloperOps/prepararEstacao.sh
+++ estacaoDeveloperOps/prepararEstacao.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
+#Garante que o interpretador utilizado será BashScript (nescessário em alguns casos)
+# Este script depende do módulo Super-Bits DevOps encontrado em: https://github.com/salviof/superBitsDevOps
+
+# Chamando funções do core
+source /home/superBits/superBitsDevOps/core/coreSBBash.sh
+#
 
 variaveisSB=/home/superBits/superBitsDevOps/VARIAVEIS/SB_VARIAVEIS.sh
 source /home/superBits/superBitsDevOps/VARIAVEIS/SB_VARIAVEIS.sh
 
 echo "$caminhoFrameWork"
-echo "Bem Vindo ao sistema de Softwares Super-Bits"
-echo "Somos Entusiastas do Open-source "
-echo "Antes de começar, você precisa instalar o NetBeans, e o JAVA 8"
-echo "só contiue a execução caso tenha o Java 1.8 e o Netbens instalado, ok? (ctr+c para sair)"
+alerta "Bem Vindo ao sistema de Softwares Super-Bits"
+alerta "Somos Entusiastas do Open-source "
+alerta "Antes de começar, você precisa instalar o NetBeans, e o JAVA 8"
+alerta "só contiue a execução caso tenha o Java 1.8 Maven Git, e o Netbens instalado, ok? (ctr+c para sair)"
+alerta "Caso não tenha instalado, execute o script instalar aplicativos antes  (ctr+c para sair)"
 pause
-echo "Para proseguir, precisamos de algumas informações básicas."
-echo "Qual o Seu nome?"
+alerta "Para proseguir, precisamos de algumas informações básicas."
+alerta "Qual o Seu nome?"
 read nome
-echo "Qual seu e-mail cadastrado no Git HUB?"
+alerta "Qual seu e-mail cadastrado no Git HUB?"
 read email
-echo "Ok, $nome, podemos começar, o script será executado até o final a partir de agora."
-echo "Executaremos as seguintes tarefas, leia e veja se concorda antes de executar:"
-echo "1- criação do diretorio $caminhoSourceSB"
-echo "2- Sincronização com o Framework neste diretorio, com a url: $urlRepositorioSBFW"
-echo "Alteração em Seu nome e Email configurado no git para declaração de autor no git"
-echo "Alteração no git para salvar seu usuário e senha em cache por X horas"
-echo "Tecle  enter se tiver de acordo, ou ctr + c para sair"
-echo "Voce pode alterar as variaveis deste Script em $variaveisSB "
-echo "não se acanhe em dar uma olhada nesses scripts, e pensar em como melhorar seu funcionamento e documentação"
-echo "TODOS OS SCRIPTS FORAM ESCRITOS PENSANDO EM DESENVOLVIMENTO COLABORATIVO"
-echo "para sair, digit ctr +c"
-echo "Preparando ambiente para mudar o mundo com open-source? [ENTER]"
+alerta "Ok, $nome, podemos começar, o script será executado até o final a partir de agora."
+alerta "Executaremos as seguintes tarefas, leia e veja se concorda antes de executar:"
+alerta "1- criação do diretorio $caminhoSourceSB"
+alerta "2- Sincronização com o Framework neste diretorio, com a url: $urlRepositorioSBFW"
+alerta "Alteração em Seu nome e Email configurado no git para declaração de autor no git"
+alerta "Alteração no git para salvar seu usuário e senha em cache por X horas"
+alerta "Tecle  enter se tiver de acordo, ou ctr + c para sair"
+alerta "Voce pode alterar as variaveis deste Script em $variaveisSB "
+alerta "não se acanhe em dar uma olhada nesses scripts, e pensar em como melhorar seu funcionamento e documentação"
+alerta "TODOS OS SCRIPTS FORAM ESCRITOS PENSANDO EM DESENVOLVIMENTO COLABORATIVO"
+alerta "para sair, digit ctr +c"
+alerta "Preparando ambiente para mudar o mundo com open-source? [ENTER]"
 pause
 git config --global user.name "$nome"
 git config --global user.email "$email"
@@ -45,19 +52,24 @@ git clone "$urlRepositorioSBFW"
 
 mkdir $PASTA_TRABALHO_USUARIO -p
 echo "TRABALHO_ATUAL=\"Tarefa não Especificada\"" > $ARQUIVO_TRABALHO_USUARIO
-#cd "$caminhoSBFW/manualImport"
-#./instalaTema.sh
-#./instalaMysql.sh
-#cd "$caminhoSBFW/EditorImagem"
-#./compilaProjeto.sh 
-#cd $caminhoSBFW
-#./compilaProjeto.sh 
+cd "$caminhoSBFW/manualImport"
+./instalaTema.sh
+./instalaMysql.sh
+cd "$caminhoSBFW/EditorImagem"
+./compilaProjeto.sh 
+cd $caminhoSBFW
+./compilaProjeto.sh 
+cd $caminhoSourceSB
+git clone https://github.com/salviof/SB_AdminTools.git
+cd $caminhoSourceSB/SB_AdminTools
+git clone https://github.com/salviof/SB_CRIADOR_COMPONENTE.git
+cd 
 
 echo " ****** VERIFICANDO SSH ****** "
 
 echo "CAMINHO QUE SERÁ VERIFICADO " /home/$SUDO_USER/.ssh/*pub
 read
-
+ssh-keygen
 if ! ls /home/$SUDO_USER/.ssh/*pub
         then
        	echo "Parece que você ainda não tem uma chave publica de ssh :(.."
