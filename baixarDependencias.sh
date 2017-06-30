a8cb5c15e514cf16433f919fe51d5ac291f4d6cf
salvio <salvio@localhost.localdomain>
2017-06-30 01:06:25 -0300

    Atualização atendendendo:

--- baixarDependencias.sh
+++ baixarDependencias.sh
@@ -68,6 +68,12 @@ mvn clean install    -Dmaven.test.skip=true
 cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/SBProjetos/model_regras/
 mvn clean install    -Dmaven.test.skip=true
 
+cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/SBProjetos/SBComandos
+mvn clean install    -Dmaven.test.skip=true
+
+echo "java -jar /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/SBProjetos/SBComandos/target/SBComandos-0.9.one-jar.jar" > /home/superBits/SBConsole.sh
+cd /home/superBits
+chmod +x ./SBConsole.sh
 
 
 
