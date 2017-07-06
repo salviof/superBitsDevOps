9a73cd0ac97611edd765f89c913c5d860cae5e93
Salvio Furbino Elias <salviof@gmail.com>
2017-07-06 05:30:31 -0300

    Atualizacao automática

--- baixarDependencias.sh
+++ baixarDependencias.sh
@@ -48,6 +48,11 @@ alerta "Compilando Editor Imagem"
 cd  /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/utilitarios/EditorImagem/
 mvn clean install -Dmaven.test.skip=true
 
+alerta "Compilando Editor de Arquivos de Usuários"
+cd  /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/utilitarios/EditorImagem/
+mvn clean install -Dmaven.test.skip=true
+
+
 
 alerta "Compilando SB PErsistencia"
 cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/SB_FRAMEWORK/SBPersistencia
@@ -64,12 +69,22 @@ mvn clean install    -Dmaven.test.skip=true
 alerta "Compilando utilitário SBShell"
 cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/utilitarios/SBShell/shellCommands
 mvn clean install    -Dmaven.test.skip=true
-
+alerta "compilando SBProjetos"
 cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/SBProjetos/model_regras/
 mvn clean install    -Dmaven.test.skip=true
-
+alerta "Compilando SBComandos"
 cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/SBProjetos/SBComandos
 mvn clean install    -Dmaven.test.skip=true
+alerta "Compilando Módulo Controle de usuários padrão "
+cd /home/superBits/projetos/Super_Bits/source/Controle_Usuario_Basico/
+mvn clean install    -Dmaven.test.skip=true
+
+alerta "Combinando Módulo Admin Tools"
+cd /home/superBits/projetos/Super_Bits/source/SB_AdminTools/modelRegras/
+mvn clean install    -Dmaven.test.skip=true
+cd /home/superBits/projetos/Super_Bits/source/SB_AdminTools/webApp
+mvn clean install    -Dmaven.test.skip=true
+#./empacotarModulo.sh
 
 echo "java -jar /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/SBProjetos/SBComandos/target/SBComandos-0.9.one-jar.jar" > /home/superBits/SBConsole.sh
 cd /home/superBits
