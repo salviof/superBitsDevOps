b6c88266529ab4133f2fdacafadbb5f2b0641f5e
Salvio Furbino Elias <salviof@gmail.com>
2019-03-12 20:56:21 -0300

    Atualizacao automática

--- VARIAVEIS/SB_VARIAVEIS.sh
+++ VARIAVEIS/SB_VARIAVEIS.sh
@@ -7,15 +7,16 @@ declare -a PASTAS_MODULO=("modelRegras" "webApp" "Android" "ws" "ic")
 urlRepositorioSBFW="https://github.com/salviof/SuperBits_FrameWork"
 caminhoHome="/home/superBits"
 CAMINHO_HOME=$caminhoHome
-caminhoSBFW="$caminhoHome/projetos/Super_Bits/source/SuperBits_FrameWork"
+caminhoSBFW="$caminhoHome/projetos/coletivoJava/source/fw/FWColetivoJava"
 CAMINHO_SBFW=$caminhoSBFW
 caminhoProjetos="$caminhoHome/projetos"
 CAMINHO_PROJETOS=$caminhoProjetos
 caminhoSourceSB="$caminhoProjetos/Super_Bits/source"
 CAMINHO_SOURCE_SB=$caminhoSourceSB
-caminhoSourceFJ="$caminhoProjetos/FaculdadeJava/source"
+CAMINHO_SOURCE_COLETIVO=$caminhoProjetos/coletivoJava/source
+#caminhoSourceFJ="$caminhoProjetos/FaculdadeJava/source"
 caminhoReleaseSB="$caminhoProjetos/Super_Bits/release"
-caminhoReleaseFJ="$caminhoProjetos/FaculdadeJava/release"
+#caminhoReleaseFJ="$caminhoProjetos/FaculdadeJava/release"
 PASTA_TRABALHO_USUARIO="/home/superBits/$USER"
 ARQUIVO_TRABALHO_USUARIO="$PASTA_TRABALHO_USUARIO/trabalhoAtual.info"
 
@@ -23,6 +24,7 @@ alerta1 "VARIAVEIS DE AMBIENTE DO SISTEMA"
 alerta1 "Variavel CAMINHO_HOME=$caminhoHome"
 alerta1 "variavel CAMINHO_SBFW=$CAMINHO_SBFW"
 alerta1 "variaval CAMINHO_SOURCE_SB=$CAMINHO_SOURCE_SB"
+alerta1 "CAMINHO_SOURCE_COLETIVO=$CAMINHO_SOURCE_COLETIVO"
 alerta1 "variavel PASTA_TRABALHO_USUARIO=$PASTA_TRABALHO_USUARIO"
 alerta1 "variavel ARQUIVO_TRABALHO_USUARIO=$ARQUIVO_TRABALHO_USUARIO"
 
--- baixarDependencias.sh
+++ baixarDependencias.sh
@@ -10,70 +10,81 @@ sudo yum -y install git
 sudo yum -y install maven
 
 alerta "Criando Pasta Source do Coletivo Java"
-mkdir "$caminhoSourceSB" -p
-alerta "Criando Release do Coletivo Java"
+mkdir $CAMINHO_SOURCE_COLETIVO -p
+alerta "Criando Pasta Source para projetos de desenvolvimento do Super-Bits Framework"
+mkdir "$CAMINHO_SOURCE_SB" -p
+alerta "Criando Pasta Release para projetos de desenvolvimento do Super-Bits Framework"
 mkdir "$caminhoReleaseSB" -p
-alerta "Configurando Arquivos client.info, contendo o endereço do repositório Git do coletivo para source e release"
-echo "SERVIDOR_GIT_SOURCE=https://github.com/salviof" > "$caminhoSourceSB/cliente.info"
-echo "SERVIDOR_GIT_RELEASE=https://github.com/salviof/NAO_IMPLEMENTADO" > $caminhoReleaseSB/cliente.info
-
-
-
-mkdir /home/superBits/projetos/Super_Bits/source -p
-mkdir /home/superBits/projetos/Super_Bits/release -p
+#alerta "Configurando Arquivos client.info, contendo o endereço do repositório Git do coletivo para source e release"
+#echo "SERVIDOR_GIT_SOURCE=https://github.com/salviof" > "$caminhoSourceSB/cliente.info"
+#echo "SERVIDOR_GIT_RELEASE=https://github.com/salviof/NAO_IMPLEMENTADO" > $caminhoReleaseSB/cliente.info
+#mkdir /home/superBits/projetos/Super_Bits/source -p
+#mkdir /home/superBits/projetos/Super_Bits/release -p
 mkdir /home/superBits/recursos/ -p
 cd /home/superBits/recursos
-alerta "Clonando Coposite Componentes e configurações de Web-Inf padrão"
-git clone https://github.com/salviof/SBComp.git
-git clone https://github.com/salviof/WEB-INF.git
+
+#git clone https://github.com/salviof/SBComp.git
+#git clone https://github.com/salviof/WEB-INF.git
 cd /home/superBits/projetos/Super_Bits/source 
-alerta "Clonando Modulos Básicos Super Bits frame Work"
-git clone https://github.com/salviof/SuperBits_FrameWork.git
-alerta "Clonando Modulo Controle de Usuários Básico"
-git clone https://github.com/salviof/Controle_Usuario_Basico
-alerta "Clonando Criado de componente JSF"
+alerta "Clonando editor de componentes em Super_Bits"
 git clone https://github.com/salviof/SB_CRIADOR_COMPONENTE
-alerta "Clonando Modulo Admin Tools "  
+alerta "Clonando Modulo Admin Tools em Super_Bits"
 git clone https://github.com/salviof/SB_AdminTools
+alerta "Clonando SBProjetos em Super_Bits"
+git clone https://github.com/salviof/SB_Projetos
+alerta "Instalando Framework"
+cd $CAMINHO_SOURCE_COLETIVO
+git clone https://github.com/salviof/FWColetivoJava.git
+cd $CAMINHO_SOURCE_COLETIVO/FWColetivoJava
+./baixarDependencias.sh
+cd $CAMINHO_SOURCE_COLETIVO/FWColetivoJava
+./dependenciasErpOpen.sh
+./compilarTudo.sh
+#alerta "Clonando Modulos Básicos Super Bits frame Work"
+#git clone https://github.com/salviof/SuperBits_FrameWork.git
+#alerta "Clonando Modulo Controle de Usuários Básico"
+#git clone https://github.com/salviof/Controle_Usuario_Basico
+#alerta "Clonando Criado de componente JSF"
 
-alerta "Compilando Tema Padrão JSF"
-cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/bibliotecas
-./instalaTema.sh
+
+#alerta "Compilando Tema Padrão JSF"
+#cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/bibliotecas
+#./instalaTema.sh
 
 
-alerta "Compilando FrameWork"
+#alerta "Compilando FrameWork"
    
-cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/SB_FRAMEWORK
-mvn clean install    -Dmaven.test.skip=true
-alerta "Compilando utilitário SBShell"
-cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/utilitarios/SBShell/shellCommands
-mvn clean install    -Dmaven.test.skip=true
+#cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/SB_FRAMEWORK
+#mvn clean install    -Dmaven.test.skip=true
+#alerta "Compilando utilitário SBShell"
+#cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/utilitarios/SBShell/shellCommands
+#mvn clean install    -Dmaven.test.skip=true
 
-alerta "Compilando model Regras do projeto"
-cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/SBProjetos/model_regras
-mvn -DskipTests=true clean install
+#alerta "Compilando model Regras do projeto"
+#cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/SBProjetos/model_regras
+#mvn -DskipTests=true clean install
 
 
 
-alerta "Compilando SBComandos"
-cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/SBProjetos/SBComandos
-mvn clean install    -Dmaven.test.skip=true
-alerta "Compilando Módulo Controle de usuários padrão "
-cd /home/superBits/projetos/Super_Bits/source/Controle_Usuario_Basico/
-mvn clean install    -Dmaven.test.skip=true
+#alerta "Compilando SBComandos"
+#cd /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/SBProjetos/SBComandos
+#mvn clean install    -Dmaven.test.skip=true
+#alerta "Compilando Módulo Controle de usuários padrão "
+#cd /home/superBits/projetos/Super_Bits/source/Controle_Usuario_Basico/
+#mvn clean install    -Dmaven.test.skip=true
 
-alerta "Combinando Módulo Admin Tools"
-cd /home/superBits/projetos/Super_Bits/source/SB_AdminTools/modelRegras/
-mvn clean install    -Dmaven.test.skip=true
-cd /home/superBits/projetos/Super_Bits/source/SB_AdminTools/webApp
+#alerta "Combinando Módulo Admin Tools"
+#cd /home/superBits/projetos/Super_Bits/source/SB_AdminTools/modelRegras/
+#mvn clean install    -Dmaven.test.skip=true
+#cd /home/superBits/projetos/Super_Bits/source/SB_AdminTools/webApp
 #mvn clean install    -Dmaven.test.skip=true
 #./empacotarModulo.sh
 
 
 alerta "--Executando SBConcole"
-echo "java -jar /home/superBits/projetos/Super_Bits/source/SuperBits_FrameWork/SBProjetos/SBComandos/target/SBComandos-0.9.one-jar.jar" > /home/superBits/SBConsole.sh
+echo "java -jar /home/superBits/projetos/Super_Bits/source/SBProjetos/SBComandos/target/SBComandos-0.9.one-jar.jar" > /home/superBits/SBConsole.sh
 cd /home/superBits
 chmod +x ./SBConsole.sh
 
-./SBConsole.sh
+#./SBConsole.sh
 
