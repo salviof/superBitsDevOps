2f229b8aa5487fb26dd47653ec025b5647e063a7
Salvio Furbino <salviof@gmail.com>
2019-03-30 13:58:29 -0300

    Atualizacao automática

--- baixarDependencias.sh
+++ baixarDependencias.sh
@@ -14,10 +14,11 @@ git clone https://github.com/salviof/FWColetivoJava
 mkdir /home/superBits/projetos/coletivoJava/source/erpColetivoJava/ -p 
 cd /home/superBits/projetos/coletivoJava/source/erpColetivoJava/
 git clone https://github.com/salviof/apiComunicacao.git  
-git clone https://github.com/salviof/implementacaoPadraoComunicacao.git
 git clone https://github.com/salviof/SBErpGatewayPagamento.git
 cd /home/superBits/projetos/coletivoJava/source/
 mkdir /home/superBits/projetos/Super_Bits/source/ -p
+cd /home/superBits/projetos/Super_Bits/source/
+git clone https://github.com/salviof/SB_CRIADOR_COMPONENTE.git
 git clone https://github.com/salviof/Controle_Usuario_Basico.git
 git clone https://github.com/salviof/SB_AdminTools.git
 git clone https://github.com/salviof/SBProjetos.git
