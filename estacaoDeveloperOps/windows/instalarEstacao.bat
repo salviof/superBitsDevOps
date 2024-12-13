3eaa0a28bc245d87ec7087acb229ced6589bb9c6
Salvio Furbino <salviof@gmail.com>
2024-12-13 12:49:39 -0300

    Atualizacao automática

--- estacaoDeveloperOps/windows/instalarEstacao.bat
+++ estacaoDeveloperOps/windows/instalarEstacao.bat
@@ -1,16 +1,16 @@
 @echo off
-rem Verifica se a variável CLIENTE_BASE existe
+rem Verifica se a variÃ¡vel CLIENTE_BASE existe
  if not defined COLETIVO_JAVA_WORKSPACE (
-    echo A variávelCOLETIVO_JAVA_WORKSPACE não existe. Criando agora...
-	rem Solicita o nome do usuário e define "Desconhecido" como valor padrão
-	set /p diretorioPadrao= Deseja personalizar o seu diret�rio de trabalho? [%USERPROFILE%\coletivo_java]: 
+    echo A variÃ¡velCOLETIVO_JAVA_WORKSPACE nÃ£o existe. Criando agora...
+	rem Solicita o nome do usuÃ¡rio e define "Desconhecido" como valor padrÃ£o
+	set /p diretorioPadrao= Deseja personalizar o seu diretÃrio de trabalho? [%USERPROFILE%\coletivo_java]: 
 	rem
-	rem rem Se o usuário não digitar nada, o valor padrão será atribuído
+	rem rem Se o usuÃ¡rio nÃ£o digitar nada, o valor padrÃ£o serÃ¡ atribuÃ­do
 	if "%diretorioPadrao%"=="" set diretorioPadrap=%USERPROFILE%\coletivo_java
         setx COLETIVO_JAVA_WORKSPACE "%USERPROFILE%\coletivo_java" /M
         echo COLETIVO_JAVA_WORKSPACE crida com sucesso
      ) else (
-         echo variavel de ambiente COLETIVO_JAVA_WORKSPACE já existe com o valor: %COLETIVO_JAVA_WORKSPACE%
+         echo variavel de ambiente COLETIVO_JAVA_WORKSPACE jÃ¡ existe com o valor: %COLETIVO_JAVA_WORKSPACE%
      )
 pause
 
@@ -20,8 +20,8 @@ git clone https://github.com/salviof/superBitsDevOps.git
 cd superBitsDevOps
 git pull
 cd estacaoDeveloperOps\docker
-echo "Legal, já configuramos a sua pasta de trabalho e baixamos os scritps de deve ops, no preposit�rio https://github.com/salviof/superBitsDevOps.git "
-echo "Agora Vamos compilar a imagem docker que ir� realizar todas opera��es de devops"
+echo "Legal, jÃ¡ configuramos a sua pasta de trabalho e baixamos os scritps de deve ops, no prepositÃrio https://github.com/salviof/superBitsDevOps.git "
+echo "Agora Vamos compilar a imagem docker que irÃ realizar todas operaÃÃes de devops"
 echo "seus projetos devem ser criados em  %COLETIVO_JAVA_WORKSPACE%\projetos\nomeDaEmpresa\source\nomeProjeto"
 
 pause 
