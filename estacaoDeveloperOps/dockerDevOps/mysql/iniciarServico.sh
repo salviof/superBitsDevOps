9dc595190acd4d646ef61a458694d6a6adec54c8
Salvio Furbino <salvio@localhost.localdomain>
2025-01-06 12:44:20 -0300

    Atualizacao automática

--- estacaoDeveloperOps/dockerDevOps/mysql/iniciarServico.sh
+++ estacaoDeveloperOps/dockerDevOps/mysql/iniciarServico.sh
@@ -1,4 +1,4 @@
 sudo systemctl start docker
 sudo docker stop devopsTestes1
 sudo docker rm devopsTestes1
-sudo docker run -d  --volume /home/superBits/projetos/flinster/source/laudosFlinster:/devopsDBScript/:z  -e MYSQL_ROOT_PASSWORD='senhaDev#123' -e MYSQL_DATABASE='mydb' -e  MYSQL_ALLOW_EMPTY_PASSWORD='yes' -e MYSQL_PASSWORD='senhaDev#123' -e MYSQL_ROOT_HOST="%" -p 3306:3306 --name devopsTestes1  coletivo_java_devops:mysql57 
+sudo docker run -d  --volume /home/superBits/desenvolvedor/bancoDeDados/laudosFlinster:/devopsDBScript/:z  -e MYSQL_ROOT_PASSWORD='senhaDev#123' -e MYSQL_DATABASE='mydb' -e  MYSQL_ALLOW_EMPTY_PASSWORD='yes' -e MYSQL_PASSWORD='senhaDev#123' -e MYSQL_ROOT_HOST="%" -p 3306:3306 --name devopsTestes1  coletivo_java_devops:mysql57 
